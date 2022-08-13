570cd5816c2197ece0f12d247074049e93200c40
AnwarChong <AnwarBinBujang@users.noreply.github.com>
2022-08-14 01:03:42 +0800

    Hello

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,18 +2,18 @@
 @extends('layouts.navbars.navs.guest')
 @yield('content')
 @section('content')
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                <li data-target="#myCarousel" data-slide-to="1"></li>
-                <li data-target="#myCarousel" data-slide-to="2"></li>
-            </ol>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
 
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner">
-                <div class="item active">
-                     <div class="card-body" style="
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <div class="card-body" style="
                        -ms-flex: 1 1 auto;
                       position: absolute;
                       border-radius: 6px;
@@ -21,71 +21,78 @@
                       bottom: 42%;
                       left: 10%;
                       z-index: 50;
-                      padding-top: 30px;
+                      padding-top: 15px;
                       padding-left: 30px;
                       background: rgba(0,0,0,.30);">
-                        <h5 class="card-title" style="font-size: 40px; color: #ffffff">Card title</h5>
-                        <p class="card-text" style="font-size: 14px; color: #ffffff">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                         <button type="button" class="btn pull-right" style="font-size: 14px; color: #ffffff; background-color: #FF0000">
-                             View More <i class="glyphicon glyphicon-chevron-right"></i>
-                         </button>
-{{--                         <button href="#" class="btn pull-right" style="font-size: 14px; background-color: #FF0000; color: #ffffff"> Go somewhere--}}
-{{--                             <i class="fa fa-home pull-left"></i> Home--}}
-{{--                             </button>--}}
-                     </div>
-                    <img src="https://www.publicbankgroup.com/CMSPages/GetFile.aspx?guid=c79d77f6-6779-4025-b83a-e937bce893f5" style="
+                    <h5 class="card-title" style="font-size: 35px; color: #ffffff">Public Bank Stock Price</h5>
+                    <p class="card-text" style="font-size: 60px; color: #ffffff; font-weight: bold">RM 4.6451</p>
+                    <p style="font-size: 16px; color: #ffffff;
+                       position:absolute;
+                       padding-top: 20px;
+                       bottom:-10px;
+
+                       ">Stock Price as of 06 Aug 2022 5:00PM</p>
+
+                    <button type="button" class="btn pull-right" style="font-size: 14px; color: #ffffff; background-color: #FF0000">
+                        View More <i class="glyphicon glyphicon-chevron-right"></i>
+                    </button>
+                    {{--                         <button href="#" class="btn pull-right" style="font-size: 14px; background-color: #FF0000; color: #ffffff"> Go somewhere--}}
+                    {{--                             <i class="fa fa-home pull-left"></i> Home--}}
+                    {{--                             </button>--}}
+                </div>
+                <img src="https://www.publicbankgroup.com/CMSPages/GetFile.aspx?guid=c79d77f6-6779-4025-b83a-e937bce893f5" style="
                     filter: brightness(80%);
                     object-fit: cover; /* Do not scale the image */
                     object-position: top; /* Center the image within the element */
                     width:100%;
                     height: 95vh;
                     display: inline-block;">
-                    <div class="carousel-caption">
-                        <h3>Los Angeles</h3>
-                        <p>LA is always so much fun!</p>
-                    </div>
-                    </div>
-
-                <div class="item" >
-                    <img src="https://www.publicbankgroup.com/CMSPages/GetFile.aspx?guid=85398e62-5c20-43f9-8f2c-6a2b42b9259a" style="
-                    filter: brightness(100%);
-                    object-fit: cover; /* Do not scale the image */
-                    object-position: top; /* Center the image within the element */
-                    width:100%;
-                    height: 95vh;
-                    display: inline-block;">
-
-                    <div class="carousel-caption">
-                        <h3>Chicago</h3>
-                        <p>Thank you, Chicago!</p>
-                    </div>
-                </div>
-
-                <div class="item" >
-                    <img src="https://www.publicbankgroup.com/CMSPages/GetFile.aspx?guid=a3287894-0379-4783-9297-47fa18a5b8d7" style="
-                    filter: brightness(100%);
-                    object-fit: cover; /* Do not scale the image */
-                    object-position: top; /* Center the image within the element */
-                    width:100%;
-                    height: 95vh;
-                    display: inline-block;">
-                    <div class="carousel-caption">
-                        <h3>New York</h3>
-                        <p>We love the Big Apple!</p>
-                    </div>
+                <div class="carousel-caption">
+                    <h3>Los Angeles</h3>
+                    <p>LA is always so much fun!</p>
                 </div>
             </div>
 
-            <!-- Left and right controls -->
-            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right"></span>
-                <span class="sr-only">Next</span>
-            </a>
+            <div class="item" >
+                <img src="https://www.publicbankgroup.com/CMSPages/GetFile.aspx?guid=85398e62-5c20-43f9-8f2c-6a2b42b9259a" style="
+                    filter: brightness(100%);
+                    object-fit: cover; /* Do not scale the image */
+                    object-position: top; /* Center the image within the element */
+                    width:100%;
+                    height: 95vh;
+                    display: inline-block;">
+
+                <div class="carousel-caption">
+                    <h3>Chicago</h3>
+                    <p>Thank you, Chicago!</p>
+                </div>
+            </div>
+
+            <div class="item" >
+                <img src="https://www.publicbankgroup.com/CMSPages/GetFile.aspx?guid=a3287894-0379-4783-9297-47fa18a5b8d7" style="
+                    filter: brightness(100%);
+                    object-fit: cover; /* Do not scale the image */
+                    object-position: top; /* Center the image within the element */
+                    width:100%;
+                    height: 95vh;
+                    display: inline-block;">
+                <div class="carousel-caption">
+                    <h3>New York</h3>
+                    <p>We love the Big Apple!</p>
+                </div>
+            </div>
         </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
 
 
 
@@ -95,15 +102,15 @@
     </div>
 
     <script>
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-    })
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        })
 
-    // Initialize popover component
-    $(function () {
-        $('[data-toggle="popover"]').popover()
-    })
-</script>
+        // Initialize popover component
+        $(function () {
+            $('[data-toggle="popover"]').popover()
+        })
+    </script>
 @endsection
 
 
